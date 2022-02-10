0cc936087a666272ff0ba9ce1d4e3ec254f43b4c
tanveer <mdtanviralamht731@gmail.com>
2022-02-10 20:49:26 +0600

    delete-btn added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const inputValue = document.getElementById('input-value');
 const addButton = document.getElementById('add-btn');
 const parentItem = document.getElementById('parent-item');
 const successButton = document.getElementById('success-btn');
-const dangerButton = document.getElementById('danger-btn');
+const allClearButton = document.getElementById('all-clear-btn');
 
 
 // items addition event 
@@ -13,31 +13,62 @@ addButton.addEventListener('click',function(){
     }else if(inputValue.value.length <3){
         alert('Item name 3 character greater than!');
     }
-    else if(inputValue.value.length > 20){
+    else if(inputValue.value.length > 15){
         alert('Item name  20 character less than !');
     }
 
     else{
         const othersItem = document.createElement('div');
     othersItem.innerHTML = `
-                        <div class="item flex justify-between pt-2  items-center">
-                        <div class="item-icons">
-                            <div class="h-3 w-3 bg-red-600 rounded-2xl"></div>
-                        </div>
-                        <div class="item-text">
-                            <span class=" text-1xl md:text-2xl text-white font-normal md:font-bold  tracking-widest ">${inputValue.value}</span>
-                        </div>
-                        <div class="item-action flex items-center">
-                            <button id="success-btn">
-                                <img src="./image/success-icon-png-22.jpg" class="w-6 md:w-8 mr-2 ease-out duration-300 hover:opacity-[.8] " alt="">
-                            </button>
-                            <button id="danger-btn">
-                                <img src="./image/cross-removebg-preview.png" class="w-6 md:w-8 ease-out duration-300 hover:opacity-[.8]" alt="">
-                            </button>
-                        </div>
-    
-                    </div>`;
+                <div id="my-item" class="item flex justify-between pt-2  items-center">
+                <div class="h-3 w-3 bg-red-600 rounded-2xl"></div>
+                <span class=" capitalize text-1xl md:text-2xl text-white font-normal md:font-bold  tracking-widest ">${inputValue.value}</span>
+                <div class="item-action flex items-center">
+                    <button  class="success-btn">
+                        <img src="./image/success-icon-png-22.jpg" class="w-6 md:w-8 mr-2 ease-out duration-300 hover:opacity-[.8] " alt="">
+                    </button>
+                    <button class="delete-btn">
+                        <img src="./image/cross-removebg-preview.png" class="w-6 md:w-8 ease-out duration-300 hover:opacity-[.8]" alt="">
+                    </button>
+                </div>
+            </div>
+                    `;
     parentItem.appendChild(othersItem);
     inputValue.value = '';
+
+
+    const deleteButton = document.getElementsByClassName("delete-btn");
+console.log(deleteButton);
+    for (const button of deleteButton) {
+      button.addEventListener("click", function (e) {
+          console.log('hello');
+        e.target.parentNode.parentNode.parentNode.style.display = "none";
+        // console.log(e.target.parentNode.parentNode);
+      });
+    }
     }
 });
+// all clear event 
+allClearButton.addEventListener('click',function (){
+    const shopItems = document.getElementsByClassName('item');
+    for(const shopItem of shopItems){
+        shopItem.style.display = 'none';
+    }
+})
+// successButton event
+// const dangerButton = document.getElementsByClassName('danger-btn');
+// console.log(dangerButton);
+// for( const dangerBtn of dangerButton ){
+//     console.log(dangerBtn)
+//     dangerBtn.addEventListener('click', function (e){
+//         console.log('hello');
+//         // event.target.parentNode.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode.parentNode);
+//         // e.target.parentNode.parentNode.parentNode.style.display = "none";
+
+//     })
+
+// }
+
+
+
+
